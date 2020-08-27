b248072890d75f1c21d9be4fcc50ae5e90ec02f9
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-08-27 16:22:32 +0500

    Gender commit

--- Fitness.BL/Model/Gender.cs
+++ Fitness.BL/Model/Gender.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fitness.BL.Model
 {
+    /// <summary>
+    /// Пол
+    /// </summary>
     class Gender
     {
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string Name { get; }
+        /// <summary>
+        /// Создать новый пол
+        /// </summary>
+        /// <param name="_name">Имя пола</param>
+        public Gender(string _name)
+        {
+            if(string.IsNullOrWhiteSpace(_name))
+            {
+                throw new ArgumentNullException("Name is not can be null or with white space");
+            }
+            Name = _name;
+        }
+        /// <summary>
+        /// Переназначенный ToString 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
