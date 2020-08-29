44b7d11e088f049c8d6d892c4c3b76675eb199fb
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-08-29 19:28:19 +0500

    add testing method

---
+++ Fitness.BLTests/Controller/UserControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fitness.BL.Controller;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness.BL.Controller.Tests
+{
+    [TestClass()]
+    public class UserControllerTests
+    {
+        [TestMethod()]
+        public void SetNewUserDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveTest()
+        {
+            // Arrange (Данные на вход)
+            var userName = Guid.NewGuid().ToString();
+
+            // Act (Действие)
+            var controller = new UserController(userName);
+
+            // Assert (Выход, результат)
+            Assert.AreEqual(userName, controller.CurrentUser.Name);
+        }
+    }
+}
