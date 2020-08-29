cc1aaafb1ae1df88a7add2474619210e3133e1fe
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-08-29 22:48:52 +0500

    eating class & method

---
+++ Fitness.BL/Model/Eating.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fitness.BL.Model
+{
+    /// <summary>
+    /// Приём пищи
+    /// </summary>
+    public class Eating
+    {
+        public DateTime  MomentOfEating { get; }
+
+        public Dictionary<Food, double> Foods { get; }
+
+        public User User { get; }
+
+        public Eating(User user)
+        {
+            User = user ?? throw new ArgumentNullException("User can not be null", nameof(user));
+        }
+    }
+}
