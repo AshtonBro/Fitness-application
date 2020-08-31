b979d5f113a98c20069cd44dff7b0397b80f5020
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-08-31 12:43:41 +0500

    Class Eating is done

--- Fitness.BL/Model/Eating.cs
+++ Fitness.BL/Model/Eating.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Fitness.BL.Model
 {
@@ -17,8 +18,27 @@ namespace Fitness.BL.Model
         public Eating(User user)
         {
             User = user ?? throw new ArgumentNullException("User can not be null", nameof(user));
-            MomentOfEating = DateTime.Now;
+            MomentOfEating = DateTime.UtcNow;
             Foods = new Dictionary<Food, double>();
+        }
+
+        /// <summary>
+        /// Возможность добавлять продукт питания
+        /// </summary>
+        /// <param name="food">Наименование продукта</param>
+        /// <param name="calorie">Калории</param>
+        public void Add(Food food, double calorie)
+        {
+            var product = Foods.Keys.FirstOrDefault(f => f.Name.Equals(food.Name));
+
+            if(product == null)
+            {
+                Foods.Add(food, calorie);
+            }
+            else
+            {
+                Foods[product] += calorie;
+            }
         }
     }
 }
