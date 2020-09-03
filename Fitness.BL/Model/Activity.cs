eeb2316153341c51677d601eefac995de3937218
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-09-03 17:19:28 +0500

    Create Activity Class

--- Fitness.BL/Model/Activity.cs
+++ Fitness.BL/Model/Activity.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fitness.BL.Model
 {
     class Activity
     {
+        /// <summary>
+        /// Название активности
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Количество сжигаемых калорий на единицу времени
+        /// </summary>
+        public double CaloriesPerMinute { get; set; }
+
+        /// <summary>
+        /// Конструктор инициализации активности
+        /// </summary>
+        /// <param name="name">Название активности</param>
+        /// <param name="caloriesPerMinute">Кол-во с жженных калорий в минуту</param>
+        public Activity(string name, double caloriesPerMinute)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Name of user can not be null or with white space");
+            }
+
+            Name = name;
+            CaloriesPerMinute = caloriesPerMinute;
+        }
+
+        /// <summary>
+        /// Пересоздали ToString() метод  
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"Активность: {Name}, Уничтожено кКал в минуту: {CaloriesPerMinute}";
+        }
     }
 }
