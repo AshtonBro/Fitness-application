500ee712d947e46d6552b8a1440ca862eee98cb1
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-09-04 11:10:20 +0500

    Class Exercise

---
+++ Fitness.BL/Model/Exercise.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+    public class Exercise
+    {
+        /// <summary>
+        /// Дата начала выполнения упражнения
+        /// </summary>
+        public DateTime Start { get; }
+
+        /// <summary>
+        /// Дата окончания выполнения упражнения
+        /// </summary>
+        public DateTime Finish { get; }
+
+        /// <summary>
+        /// Выполняемая активность (упражнение)
+        /// </summary>
+        public Activity Activity { get; }
+
+        /// <summary>
+        /// Кто делал упражнения
+        /// </summary>
+        public User User { get; }
+
+        /// <summary>
+        /// Конструктор упражнения
+        /// </summary>
+        /// <param name="start">Время начала</param>
+        /// <param name="finish">Время окончания</param>
+        /// <param name="activity">Вид упражнения</param>
+        /// <param name="user">Исполнитель упражнения</param>
+        public Exercise(DateTime start, DateTime finish, Activity activity, User user)
+        {
+            // Проверка входных параметров
+            Start = start;
+            Finish = finish;
+            Activity = activity;
+            User = user;
+        }
+    }
+}
