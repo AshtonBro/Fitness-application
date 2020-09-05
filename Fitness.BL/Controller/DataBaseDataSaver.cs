db91de12cb6dcd01776898d51823718c6ae51269
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-09-05 13:33:36 +0500

    Add Entyti

--- Fitness.BL/Controller/DataBaseDataSaver.cs
+++ Fitness.BL/Controller/DataBaseDataSaver.cs
@@ -1,12 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fitness.BL.Controller
 {
-    class DataBaseDataSaver
+    public class DataBaseDataSaver : IDataSaver
     {
+        public T Load<T>(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(string fileName, object item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
