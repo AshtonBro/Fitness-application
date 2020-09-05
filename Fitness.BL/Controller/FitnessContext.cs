e32ec5525ebb5a41b5cde2404febde3c2403b855
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-09-05 13:54:37 +0500

    New Entity and connection string

---
+++ Fitness.BL/Controller/FitnessContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using Fitness.BL.Model;
+
+namespace Fitness.BL.Controller
+{
+    class FitnessContext : DbContext
+    {
+        public FitnessContext() : base("DBconnection") { }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
